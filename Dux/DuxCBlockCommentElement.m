a103ec3ed08226d68232fced7d238aaa315b77a2
Abhi Beckert <abhi@ma.local>
2011-12-02 05:35:39 +1000

    fix my name in some comment blocks

--- Dux/DuxCBlockCommentElement.m
+++ Dux/DuxCBlockCommentElement.m
@@ -2,7 +2,7 @@
 //  DuxCBlockCommentElement.m
 //  Dux
 //
-//  Created by Woody Beckert on 2011-11-25.
+//  Created by Abhi Beckert on 2011-11-25.
 //  
 //  This is free and unencumbered software released into the public domain.
 //  For more information, please refer to <http://unlicense.org/>
--- Dux/DuxPreferencesWindowController.m
+++ Dux/DuxPreferencesWindowController.m
@@ -2,7 +2,7 @@
 //  DuxPreferencesWindowController.m
 //  Dux
 //
-//  Created by Woody Beckert on 2011-12-01.
+//  Created by Abhi Beckert on 2011-12-01.
 //  
 //  This is free and unencumbered software released into the public domain.
 //  For more information, please refer to <http://unlicense.org/>
